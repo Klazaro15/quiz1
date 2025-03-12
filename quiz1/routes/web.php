39d09af142b234bce8cc004330030ec9138b82e3
Klazaro15 <156405187+Klazaro15@users.noreply.github.com>
2025-03-12 09:38:52 +0000

    Quiz1 Final

--- quiz1/routes/web.php
+++ quiz1/routes/web.php
@@ -6,10 +6,10 @@ use path\to\model\file;
 
 
 
-Route::resource('/inventory', InventoryController::class);
-
 Route::get('/hello/{name}', function ($name) {
     return "Hi, $name";
 
-    Route::resource('/inventory', InventoryController::class);
 });
+
+Route::resource('/inventory', InventoryController::class);
+
